47f4b9737d2d3b2488391ec835ae05e965b3bf72
0seony <yseon406@gmail.com>
2022-01-04 19:58:42 +0900

    update mission

--- src/Mission.java
+++ src/Mission.java
@@ -3,10 +3,21 @@ import java.util.Scanner;
 public class Mission {
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
-		String[] input = sc.nextLine().split(",");
+		
+		// 추가 요구사항 1
+		int input1 = sc.nextInt();
+		
+		for (int i = 1; i <= input1; i++) {
+			System.out.println(input1 + "*" + i + "=" + input1 * i);
+		}
+		
+		sc.nextLine();
+		
+		// 추가 요구사항 2
+		String[] input2 = sc.nextLine().split(",");
 
-		int A = Integer.parseInt(input[0]);
-		int B = Integer.parseInt(input[1]);
+		int A = Integer.parseInt(input2[0]);
+		int B = Integer.parseInt(input2[1]);
 
 		for (int i = 2; i <= A; i++) {
 			for (int j = 1; j <= B; j++) {
